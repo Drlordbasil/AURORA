af42ea2bed9f8467ce36ed6ac603f659e25bf1a9
Lord Basil - Automate EVERYTHING <126736516+Drlordbasil@users.noreply.github.com>
2024-06-15 09:39:32 -0400

    Update function_calling.py

--- function_calling.py
+++ function_calling.py
@@ -1,27 +1,11 @@
-# import json
-# import subprocess
-# import os
-# import time
-# import requests
-# from groq import Groq
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.chrome.service import Service as ChromeService
-# from webdriver_manager.chrome import ChromeDriverManager
-# from bs4 import BeautifulSoup
-# from nltk.tokenize import sent_tokenize
-# from textblob import TextBlob
-# from image_vision import ImageVision
-# from pdfminer.high_level import extract_text
+
 from kivy.clock import Clock
 from llm_api_calls import LLM_API_Calls
 class FunctionCalling:
-    # MODEL = 'llama3-70b-8192'
+
 
     def __init__(self, status_update_callback):
-        #  self.client = ""#Groq(api_key=api_key)
-        #  self.image_vision = ""#ImageVision() 
+
          self.status_update_callback = status_update_callback
 
     def _update_status(self, message):
